51f6ba3e373f757a9ab6a7512cb9f60b647752c9
akuraru <akuraru+bb@gmail.com>
2012-08-26 08:55:36 +0900

    目的だった機能を実装

--- csv2plist.scala
+++ csv2plist.scala
@@ -8,50 +8,82 @@ import au.com.bytecode.opencsv.CSVReader
 import scala.io.Source
 
 object csv2plist {
-  def main(args : Array[String]) : Unit = { 
-    val (filename, hasTitleRow) = ArgumentCheck(args);
-    if(filename == "") return;
-    
-    val file = new File( filename );
-    if(file.exists == false) {
-      println("No such file" + filename)
-      return;
+  def main(args: Array[String]): Unit = {
+    val (filename, hasTitleRow, hasArray) = ArgumentCheck(args);
+    if (filename == "") {
+      println("Usage: scala csv2plist.scala [-t] <filename>\n");
+      println("  -t             The file has a title row.\n");
+      return ;
     }
-    val reader = new CSVReader(new InputStreamReader(new FileInputStream( file ), "utf-8" ))
 
-    val outputFilename = filename.replace(".csv",".plist");
-    val fp_out = new PrintWriter(outputFilename, "utf-8");
-    
-    fp_out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-    fp_out.write("<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n");
-    fp_out.write("<plist version=\"1.0\">\n");
-    fp_out.write("<array>\n");
-    
-    val keys = if(hasTitleRow)reader.readNext else Array[String]();
-    Iterator.continually(reader.readNext).takeWhile(_ != null).map(_.toList).foreach {
-      values =>
-      def f(l:List[String] , i:Int):String = {
-        l match{ 
-        	case h :: t => (if(hasTitleRow)"\t\t<key>" + keys(i) + "</key>\n"else"\t\t<key>keyname"+i+"</key>\n") + "\t\t<string>"+h+"</string>\n" + f(t, i+1)
-        	case Nil => ""
-        }
-      };
-      fp_out.write("\t<dict>\n" + f(values, 0) + "\t</dict>\n");  
+    val file = new File(filename);
+    if (file.exists == false) {
+      println("No such file " + filename)
+      return ;
     }
-    fp_out.write("</array>\n" + "</plist>");
+    val reader = new CSVReader(new InputStreamReader(new FileInputStream(file), "utf-8"))
+    val body = createBody(reader, hasTitleRow, hasArray)
+
+    val outputFilename = filename.replace(".csv", ".plist");
+    val fp_out = new PrintWriter(outputFilename, "utf-8");
+
+    fp_out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n" +
+      "<plist version=\"1.0\">\n" +
+      "<array>\n" +
+      body +
+      "</array>\n" +
+      "</plist>");
     fp_out.close();
-    
+
     println("Generated")
   }
-  def ArgumentCheck(args : Array[String]): (String, Boolean) = {
-    if(args.length == 1) {
-      return (args(0), false);
-    }else if(args.length == 2 && args(0) == "-t"){
-      return (args(1), true);
-    }else {
-        println("Usage: scala csv2plist.scala [-t] <filename>\n");
-        println("  -t             The file has a title row.\n");
-      ("", false);
+  def createBody(reader: CSVReader, hasTitleRow: Boolean, hasArray: Boolean): String = {
+    if (hasArray == false) {
+      val keys = if (hasTitleRow) reader.readNext else Array[String]();
+
+      Iterator.continually(reader.readNext).takeWhile(_ != null).map(_.toList).map {
+        values =>
+          def g(i: Int): String = {
+            if (hasTitleRow) "\t\t<key>" + keys(i) + "</key>\n"
+            else "\t\t<key>keyname" + i + "</key>\n"
+          }
+          def f(l: List[String], i: Int): String = {
+            l match {
+              case h :: t => g(i) + "\t\t<string>" + h + "</string>\n" + f(t, i + 1)
+              case Nil => ""
+            }
+          };
+          "\t<dict>\n" + f(values, 0) + "\t</dict>\n"
+      }.fold("")(_ + _)
+    } else {
+      val keys = reader.readNext;
+      val values = Iterator.continually(reader.readNext).takeWhile(_ != null).map(_.toList).toList
+      def f(l : List[List[String]], i : Int):String = {
+        l match {
+          case h :: t => {
+            h match {
+              case h2 :: t2 :: _ => (if(i < h2.toInt)"\t\t<string>" + t2 + "</string>\n" else "\t</array>\n" + "\t<array>\n" + "\t\t<string>" + t2 + "</string>\n") + f(t, h2.toInt)  
+              case _ => "" + f(t, i)
+            }
+          }
+          case Nil => "\t</array>\n"
+        }
+      }
+      "\t<array>\n" + f(values, -1)
+    }
+  }
+
+  def ArgumentCheck(args: Array[String]): (String, Boolean, Boolean) = {
+    if (args.length == 1) {
+      return (args(0), false, false);
+    } else if (args.length == 2) {
+      return (args(1), args(0) == "-t", args(0) == "-a");
+    } else if (args.length == 3) {
+      val t = (args(0) == "-t" || args(1) == "-t") && (args(0) == "-a" || args(1) == "-a")
+      return (if (t) args(2) else "", t, t);
+    } else {
+      ("", false, false);
     }
   }
 }
